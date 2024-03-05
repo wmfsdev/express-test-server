7e0a99d66aec5de7dbda7331d209fd27dc3f29a6
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-03-05 23:31:10 +0000

    alternative solution: multiple get requests instead of static method

--- index.js
+++ index.js
@@ -8,6 +8,37 @@ const filepath = path.join(__dirname, 'public')
 // assign static folder
 app.use(express.static(filepath))
 
+
+// non-static method, multiple gets
+
+// app.get('/', (req, res) => {
+//   res.sendFile(filepath + '/index.html')
+// })
+
+// app.get('/index.html', (req, res) => {
+//   res.sendFile(filepath + '/index.html')
+// })
+
+// app.get('/contact.html', (req, res) => {
+//   res.sendFile(filepath + '/contact.html')
+// })
+
+// app.get('/about.html', (req, res) => {
+//   res.sendFile(filepath + '/about.html')
+// })
+
+// app.get('/404.html', (req, res) => {
+//   res.sendFile(filepath + '/404.html')
+// })
+
+// app.get('/style.css', (req, res) => {
+//   res.sendFile(filepath + '/style.css')
+// })
+
+// app.get('/script.js', (req, res) => {
+//   res.sendFile(filepath + '/script.js')
+// })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
