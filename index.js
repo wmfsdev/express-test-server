04acb569d42f2e09b9f9f923b2ee5c1e3b9006d3
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-03-05 22:28:08 +0000

    use static method for middleware handling

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path")
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const filepath = path.join(__dirname, 'public')
+
+// assign static folder
+app.use(express.static(filepath))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
